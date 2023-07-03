7a703b70e9baf0821e08dfda176f1d24a038687b
Kraisser <vgaiko@mail.ru>
2023-07-04 02:08:50 +0300

    1.1 init

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,26 @@ import '../scss/common/common.scss';
 
 import '../scss/header.scss';
 import '../scss/main.scss';
-// import '../scss/main.scss';
+
+import Swiper, {Navigation, Pagination} from 'swiper';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+// import 'swiper/scss/pagination';
+
+const casesSlider = new Swiper('.cases-slider', {
+	slideClass: 'cases-slide-item',
+	slidesPerView: 3,
+	spaceBetween: 30,
+	navigation: {
+		nextEl: '.cases-button-next',
+		prevEl: '.cases-button-prev',
+	},
+	pagination: {
+		el: '.cases-pagination',
+		bulletClass: 'cases-pagination-bullet',
+		bulletActiveClass: 'cases-pagination-bullet-active',
+		type: 'bullets',
+		clickable: true,
+	},
+	modules: [Navigation, Pagination],
+});
